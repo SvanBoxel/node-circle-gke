a259411bc981438dc79f50b9abd3940357a3889a
Sebass van Boxel <svboxel@gmail.com>
2019-03-29 15:20:24 +0100

    output

--- scripts/create-deployment.sh
+++ scripts/create-deployment.sh
@@ -6,7 +6,7 @@ else
   ENVIRONMENT="qa"
 fi
 
-curl -s -v -X "POST" "https://api.github.com/repos/SvanBoxel/node-circle-gke/deployments" \
+curl -s -v -X "POST" "https://api.github.com/repos/SvanBoxel/node-circle-gke/deployments" --trace-ascii /dev/stdout \
      -H "Authorization: token ${GITHUB_TOKEN}" \
      -H 'Content-Type: application/json; charset=utf-8' \
      -H "Accept: application/vnd.github.ant-man-preview+json" \
