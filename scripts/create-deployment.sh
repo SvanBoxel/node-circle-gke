21ddf22a34e09a2de3fb487d3da49b687b7beb92
Sebass van Boxel <svboxel@gmail.com>
2019-03-29 14:51:07 +0100

    fff

--- scripts/create-deployment.sh
+++ scripts/create-deployment.sh
@@ -10,8 +10,7 @@ curl -s -X "POST" "https://api.github.com/repos/SvanBoxel/node-circle-gke/deploy
      -H "Authorization: token ${GITHUB_TOKEN}" \
      -H 'Content-Type: application/json; charset=utf-8' \
      -d $'{
-<<<<<<< Updated upstream
-  "ref": "'"${CIRCLE_BRANCH}"'",
-  "environment": "'"${ENVIRONMENT}"'" 
-  "required_contexts": []
-}' | jq -r '.id' > deployment_id
+      "ref": "'"${CIRCLE_BRANCH}"'",
+      "environment": "'"${ENVIRONMENT}"'" 
+      "required_contexts": []
+    }' | jq -r '.id' > deployment_id
