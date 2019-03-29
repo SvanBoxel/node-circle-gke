ba267ebed59a4783ecc473870b7bad3ac8eb4c54
Sebass van Boxel <svboxel@gmail.com>
2019-03-29 15:14:57 +0100

    Added accept headers

--- scripts/create-deployment.sh
+++ scripts/create-deployment.sh
@@ -9,6 +9,7 @@ fi
 curl -s -X "POST" "https://api.github.com/repos/SvanBoxel/node-circle-gke/deployments" \
      -H "Authorization: token ${GITHUB_TOKEN}" \
      -H 'Content-Type: application/json; charset=utf-8' \
+     -H "Accept: application/vnd.github.ant-man-preview+json" \
      -d $'{
       "ref": "'"${CIRCLE_BRANCH}"'",
       "environment": "'"${ENVIRONMENT}"'" ,
