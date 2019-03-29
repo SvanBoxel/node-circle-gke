95e7c6e80ddf9c510e7725234d5b267490c20498
Sebass van Boxel <svboxel@gmail.com>
2019-03-29 12:59:21 +0100

    Whoops, wrong url

--- scripts/update-deployment.sh
+++ scripts/update-deployment.sh
@@ -2,7 +2,7 @@
 
 DEPLOYMENT_ID=`cat deployment_id`
 
-curl -X "POST" "https://api.github.com/repos/SvanBoxel/node-circle-gke/${DEPLOYMENT_ID}/137129690/statuses" \
+curl -X "POST" "https://api.github.com/repos/SvanBoxel/node-circle-gke/deployments/${DEPLOYMENT_ID}/statuses" \
      -H "Authorization: token ${GITHUB_TOKEN}" \
      -H 'Content-Type: application/json; charset=utf-8' \
      -d $'{
