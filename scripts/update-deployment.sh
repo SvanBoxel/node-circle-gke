0a61558ef380a51efa36279960cdd5dafbbac24f
Sebass van Boxel <svboxel@gmail.com>
2019-03-29 14:16:49 +0100

    there we go

--- scripts/update-deployment.sh
+++ scripts/update-deployment.sh
@@ -2,9 +2,17 @@
 
 DEPLOYMENT_ID=`cat deployment_id`
 
+if [[ $CIRCLE_BRANCH=="master" ]]; then
+  ENVIRONMENT_URL="https://production.com"
+else
+  ENVIRONMENT_URL="https://staging.com"
+fi
+
 curl -X "POST" "https://api.github.com/repos/SvanBoxel/node-circle-gke/deployments/${DEPLOYMENT_ID}/statuses" \
      -H "Authorization: token ${GITHUB_TOKEN}" \
      -H 'Content-Type: application/json; charset=utf-8' \
+     -H 'Accept: application/vnd.github.ant-man-preview+json' \
      -d $'{
+  "environment_url": "'${ENVIRONMENT_URL}'"
   "state": "success"
 }'
