3644bca5dbf847785833e8f52d72f9baaf7ec4d7
Sebass van Boxel <svboxel@gmail.com>
2019-03-29 14:48:32 +0100

    set environment

--- scripts/create-deployment.sh
+++ scripts/create-deployment.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+if [[ $CIRCLE_BRANCH=="master" ]]; then
+  ENVIRONMENT="production"
+else
+  ENVIRONMENT="QA"
+fi
+
 curl -s -X "POST" "https://api.github.com/repos/SvanBoxel/node-circle-gke/deployments" \
      -H "Authorization: token ${GITHUB_TOKEN}" \
      -H 'Content-Type: application/json; charset=utf-8' \
      -d $'{
+<<<<<<< Updated upstream
   "ref": "'"${CIRCLE_BRANCH}"'",
+  "environment": "'"${ENVIRONMENT}"'" 
   "required_contexts": []
 }' | jq -r '.id' > deployment_id
