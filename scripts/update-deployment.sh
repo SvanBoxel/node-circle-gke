ead04c9fd90a714d71c2e3b138719f36a627c93e
Sebass van Boxel <svboxel@gmail.com>
2019-03-29 14:41:38 +0100

    Update ip

--- scripts/update-deployment.sh
+++ scripts/update-deployment.sh
@@ -5,7 +5,7 @@ DEPLOYMENT_ID=`cat deployment_id`
 if [[ $CIRCLE_BRANCH=="master" ]]; then
   ENVIRONMENT_URL="http://35.225.249.226/"
 else
-  ENVIRONMENT_URL="https://staging.com"
+  ENVIRONMENT_URL="https://35.225.196.189/"
 fi
 
 curl -X "POST" "https://api.github.com/repos/SvanBoxel/node-circle-gke/deployments/${DEPLOYMENT_ID}/statuses" \
