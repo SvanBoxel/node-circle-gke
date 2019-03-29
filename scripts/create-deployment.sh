ea75d59f37bffdcddc2cf5559bca417ed1bdc98f
Sebass van Boxel <svboxel@gmail.com>
2019-03-29 12:35:29 +0100

    What the token

--- scripts/create-deployment.sh
+++ scripts/create-deployment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo $GITHUB_TOKEN
 curl -X "POST" "https://api.github.com/repos/SvanBoxel/node-circle-gke/deployments" \
      -H 'Authorization: token $GITHUB_TOKEN' \
      -H 'Content-Type: text/plain; charset=utf-8' \
