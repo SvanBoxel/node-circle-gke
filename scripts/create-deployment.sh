1a6dd3fd8e8db92071792759071533b923701cb9
Sebass van Boxel <svboxel@gmail.com>
2019-03-29 12:40:01 +0100

    Use string literal

--- scripts/create-deployment.sh
+++ scripts/create-deployment.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo $GITHUB_TOKEN
+
 curl -X "POST" "https://api.github.com/repos/SvanBoxel/node-circle-gke/deployments" \
-     -H 'Authorization: token $GITHUB_TOKEN' \
-     -H 'Content-Type: text/plain; charset=utf-8' \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     -H 'Content-Type: application/json; charset=utf-8' \
      -d $'{
-  "ref": "master"
+  "ref": "master",
+  "required_contexts": []
 }'
