4f472c9d88c2819551f8abe659dd59b10dc92547
Sebass van Boxel <svboxel@gmail.com>
2019-03-29 12:20:09 +0100

    Deployment test

---
+++ .circleci/create-deployment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -X "POST" "https://api.github.com/repos/SvanBoxel/node-circle-gke/deployments" \
+     -H 'Authorization: token $GITHUB_TOKEN' \
+     -H 'Content-Type: text/plain; charset=utf-8' \
+     -d $'{
+  "ref": "master"
+}'
