b584838fdc77af779114c1cdc6bcfbb27b23502e
Sebass van Boxel <svboxel@gmail.com>
2019-04-01 10:06:35 +0200

    Include linebreaks

--- src/index.js
+++ src/index.js
@@ -14,7 +14,12 @@ const branchName = process.env.APP_BRANCH_NAME || 'N/A'
 
 app.get('/', (req, res) => {
     const welcome = myLib.helloWorld()
-    const text = `${welcome}! We're at commit ${commitRef} on branch ${branchName} which was built at ${buildDate}`
+    const text = `${welcome}! 
+
+    We're at commit ${commitRef} on branch ${branchName}.
+
+    It was built at ${buildDate}.`
+    
     res.send(text)
 })
 
