f44bd1c8981e598c99a41c28a2526c2e5a2632fd
Sebass van Boxel <svboxel@gmail.com>
2019-04-01 12:17:32 +0200

    Added layout to page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
-const fs = require('fs');
+
+app.use(express.static(__dirname + '/src'));
+app.engine('html', require('ejs').renderFile);
+app.set('views','./src');
+app.set('view engine', 'html');
 
 /*
     For the purpose of making the testing easier, we've just put this
@@ -14,25 +18,14 @@ const branchName = process.env.APP_BRANCH_NAME || 'N/A'
 
 
 app.get('/', (req, res) => {
-    fs.readFile('./index.html', (err, html) => {
-        if (err) {
-          throw err;
-        }
-
-        response.writeHeader(200, { 'Content-Type': 'text/html' });
-        response.render(__dirname + "/index.html", {welcome:'aaa'});
-        // response.write(html);
-        // response.write(`Server time: ${new Date(Date.now()).toLocaleString()}`)
-        response.end();
+    res.render('index.html', { 
+        welcome: myLib.helloWorld(),
+        commitRef,
+        branchName,
+        buildDate
     });
-    const welcome = myLib.helloWorld()
-    const text = `${welcome}! 
-
-    We're at commit ${commitRef} on branch ${branchName}.
-
-    It was built at ${buildDate}.`
-    res.send(text)
+    res.end();
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('App listening on port 3000!'))
 
